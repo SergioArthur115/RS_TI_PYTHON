70093ff9ac4f2375f19c2666298b7653f3cfa845
Sérgio Arthur Fialho M. Teixeira <182120042@senacinformatica.br>
2023-07-14 16:53:11 -0300

    exercicios aula 1

---
+++ formatNum.py
@@ -0,0 +1,4 @@
+num1 = input("Informe numero 1: ")
+num2 = input("Informe numero 2: ")
+
+print("A soma dos numeros{} e {} e soma: {} " .format(num1, num2, num1+num2))
---
+++ tiposPrimitivos.py
@@ -0,0 +1,23 @@
+nome = "Sergio"
+idade = 20
+print(nome)
+num1 = 3
+num2 = "5"
+num2 = 5
+s = num1+num2
+print(s)
+ano = 2023
+print(type(ano))
+print(type(idade))
+altura = 1.77
+peso = 80.30
+print(type(peso))
+print(type(altura))
+a = 5 + 3j
+print(type(a))
+profissao = "Programador Python"
+print(type(profissao))
+sexta = True
+feriado = False
+print(type(sexta))
+print(type(feriado))
