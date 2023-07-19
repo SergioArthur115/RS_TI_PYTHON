452c7a5efda5084223d15089908742635bbccbc9
Sérgio Arthur Fialho M. Teixeira <182120042@senacinformatica.br>
2023-07-19 16:46:47 -0300

    ajustes em exercicios

--- exercicios/mediaGeo.py
+++ exercicios/mediaGeo.py
@@ -3,6 +3,6 @@ import math
 num1=int(input("Informe o numero 1: "))
 num2=int(input("Informe o numero 2: "))
 cubo=math.pow(num2,3)
-mediaGeo=math.sqrt((num1*num2),2)
+mediaGeo=math.sqrt((num1*num2))
 print("Cubo: ",cubo)
 print("Media Geo: ",mediaGeo)
--- exercicios/qtdSalarioMin.py
+++ exercicios/qtdSalarioMin.py
@@ -1,4 +1,4 @@
 salarioMin=float(input("Informe o valor do salario minimo: "))
 salario=float(input("Informe o valor do salario mensal: "))
 qtdSalarioMin=salario/salarioMin
-print("Quantidade de salarios minimos: ", qtdSalarioMin)
+print(f"Quantidade de salarios minimos: {qtdSalarioMin:.2f}")
